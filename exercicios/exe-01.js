a7dd595d154d6840feb508184535526c115fa02f
FletserW <130801024+FletserW@users.noreply.github.com>
2023-05-27 09:46:02 -0300

    exercido 02

--- exercicios/exe-01.js
+++
@@ -1,6 +0,0 @@
-var vel = 72
-console.log(`A velocidade do seu carro é ${vel}Km/h`)
-if (vel > 60) {
-    console.log(`Voce passou da velocidade limite`)
-}
-console.log(`Dirija sempre com cuidade`)
